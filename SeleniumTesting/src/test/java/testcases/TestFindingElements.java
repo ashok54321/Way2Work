9b1716454d1b1e2b828f15bce1b1c4c98609fcb6
TB-DB <TB-DB@DESKTOP-8343GOC>
2019-03-26 19:08:34 +0530

    used ExplicitWait()

--- SeleniumTesting/src/test/java/testcases/TestFindingElements.java
+++ SeleniumTesting/src/test/java/testcases/TestFindingElements.java
@@ -3,6 +3,7 @@ package testcases;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -29,23 +30,22 @@ public class TestFindingElements {
 		//Explicit wait
 		//WebDriverWait wait = new WebDriverWait(driver, 5);
 		
-		//Fluent wait
-		/*Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-				.withTimeout(30, SECONDS)
-				.pol*/
-		
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+			
+				.withTimeout(30, TimeUnit.SECONDS)
+			       .pollingEvery(5, TimeUnit.SECONDS)
+			       .ignoring(NoSuchElementException.class);
+				
+				
 		//driver.findElement(By.id("identifierId")).sendKeys("ashokkolli.2014@gmail.com");		
 		
-		//presenceOfElement
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("identifierId"))).sendKeys("ashokkolli.2014@gmail.com");
 		
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content")).click();
 		
 		//driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("dfsdfwewr");
 		
-		//visibilityOfElement
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("fasdgagdfgg");
-		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("dfsdfwewr");
 		driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/content")).click();
 		//System.out.println(driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/content/section/div/content/div[1]/div[2]/div[2]/content")).getText());
 		System.out.println(wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/content/section/div/content/div[1]/div[2]/div[2]/content"))).getText());
