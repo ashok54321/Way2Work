da7bc0ef4fe6d2eea1fac669661e09b346fef7f9
TB-DB <TB-DB@DESKTOP-8343GOC>
2019-03-26 17:48:56 +0530

    added in Way2Work repo

---
+++ SeleniumTesting/src/test/java/testcases/TestFindingElements.java
@@ -0,0 +1,55 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestFindingElements {
+	
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://gmail.com");
+		driver.manage().window().maximize();
+		
+		//Implicit wait
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//Explicit wait
+		//WebDriverWait wait = new WebDriverWait(driver, 5);
+		
+		//Fluent wait
+		/*Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(30, SECONDS)
+				.pol*/
+		
+		//driver.findElement(By.id("identifierId")).sendKeys("ashokkolli.2014@gmail.com");		
+		
+		//presenceOfElement
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("identifierId"))).sendKeys("ashokkolli.2014@gmail.com");
+		
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content")).click();
+		
+		//driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("dfsdfwewr");
+		
+		//visibilityOfElement
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("fasdgagdfgg");
+		
+		driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/content")).click();
+		//System.out.println(driver.findElement(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/content/section/div/content/div[1]/div[2]/div[2]/content")).getText());
+		System.out.println(wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"view_container\"]/div/div/div[2]/div/div[1]/div/form/content/section/div/content/div[1]/div[2]/div[2]/content"))).getText());
+	
+	}
+
+}
---
+++ SeleniumTesting/src/test/java/testcases/testBrowsers.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testBrowsers {
+
+	public static String browser = "chrome"; // reading from excel sheet
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		if (browser.equals("chrome")) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browser.equals("Firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		}
+
+		driver.get("https://tecobytes.com");
+		String Title = driver.getTitle();
+		System.out.println(Title);
+		System.out.println(Title.length());
+		driver.close();
+		// driver.quit();
+	}
+
+}
