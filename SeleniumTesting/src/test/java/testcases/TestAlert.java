a740a8e1d5101001c364d4ab6e700a2b0f7316ee
TB-DB <TB-DB@DESKTOP-8343GOC>
2019-04-05 18:33:26 +0530

    created new classes for alerts and Iframes

---
+++ SeleniumTesting/src/test/java/testcases/TestAlert.java
@@ -0,0 +1,41 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestAlert {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[1]/div/form/div/div[6]/div[1]/input")).click();
+		
+		
+		//Using Alert interface
+		/*Alert alert=driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.accept();*/
+		
+		
+		//Using ExplicitWait
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		Alert alt=wait.until(ExpectedConditions.alertIsPresent());
+		System.out.println(alt.getText());
+		alt.accept();
+
+	}
+
+}
